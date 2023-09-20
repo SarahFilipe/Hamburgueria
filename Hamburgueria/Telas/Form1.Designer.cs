dafebb0b5fe08ad15a6feba3c1b7e5100bd624e8
Filipe <filipeabner2015@gmail.com>
2023-09-20 20:36:42 -0300

    Criação da TelaLogin - Criação da classe crypto

--- Hamburgueria/Telas/Form1.Designer.cs
+++ Hamburgueria/Telas/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hamburgueria
 {
-    partial class Form1
+    partial class TelaLogin
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.RBtnGerente = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RBtnFuncionario = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(365, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 118);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COMO COMER SEU BURGÃO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(409, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(400, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Senha:";
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtEmail.Location = new System.Drawing.Point(486, 234);
+            this.TxtEmail.Multiline = true;
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(159, 25);
+            this.TxtEmail.TabIndex = 3;
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSenha.Location = new System.Drawing.Point(486, 283);
+            this.TxtSenha.Multiline = true;
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.Size = new System.Drawing.Size(159, 25);
+            this.TxtSenha.TabIndex = 4;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.BackColor = System.Drawing.Color.White;
+            this.BtnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogin.ForeColor = System.Drawing.Color.Maroon;
+            this.BtnLogin.Location = new System.Drawing.Point(517, 345);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(102, 36);
+            this.BtnLogin.TabIndex = 5;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = false;
+            // 
+            // RBtnGerente
+            // 
+            this.RBtnGerente.AutoSize = true;
+            this.RBtnGerente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBtnGerente.ForeColor = System.Drawing.Color.Transparent;
+            this.RBtnGerente.Location = new System.Drawing.Point(549, 157);
+            this.RBtnGerente.Name = "RBtnGerente";
+            this.RBtnGerente.Size = new System.Drawing.Size(96, 28);
+            this.RBtnGerente.TabIndex = 6;
+            this.RBtnGerente.TabStop = true;
+            this.RBtnGerente.Text = "Gerente";
+            this.RBtnGerente.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(310, 451);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // RBtnFuncionario
+            // 
+            this.RBtnFuncionario.AutoSize = true;
+            this.RBtnFuncionario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RBtnFuncionario.ForeColor = System.Drawing.Color.Transparent;
+            this.RBtnFuncionario.Location = new System.Drawing.Point(414, 157);
+            this.RBtnFuncionario.Name = "RBtnFuncionario";
+            this.RBtnFuncionario.Size = new System.Drawing.Size(129, 28);
+            this.RBtnFuncionario.TabIndex = 8;
+            this.RBtnFuncionario.TabStop = true;
+            this.RBtnFuncionario.Text = "Funcionário";
+            this.RBtnFuncionario.UseVisualStyleBackColor = true;
+            // 
+            // TelaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.RBtnFuncionario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RBtnGerente);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.TxtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TelaLogin";
+            this.Text = "TelaLogin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtEmail;
+        private System.Windows.Forms.TextBox TxtSenha;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.RadioButton RBtnGerente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton RBtnFuncionario;
     }
 }
 
